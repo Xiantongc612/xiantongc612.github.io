91d5da17230e80f59d98af50cff09ce2da2c3e06
Xiantong(Shelton) Cheng <xiantong.cheng@outlook.com>
2022-09-15 08:37:14 +0800

    feat: vitepress config initialize

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+import { theme } from "./themeConfig";
+
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+const config = {
+    title: 'Xian-tong Cheng',
+    description: 'Technology makes communities different',
+    lang: 'en-US',
+    ...theme
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default config;
---
+++ docs/.vitepress/themeConfig.js
@@ -0,0 +1,9 @@
+export const theme = {
+  lastUpdated: true,
+  themeConfig: {
+    footer: {
+      message: 'Released under the CC-BY-NC-ND 4.0 License.',
+      copyright: 'Copyright © 2022-present Xian-tong Cheng'
+    }
+  }
+};
